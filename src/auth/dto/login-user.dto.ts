a78aa3e8c16990918c02c74a9a0607ceba8a18f0
Stepasha419a <Dark_Magic419a@mail.ru>
2023-01-18 21:16:31 +0300

    dtos extends, endpoint response description changes

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,12 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString, Length } from 'class-validator';
-export class LoginUserDto {
-  @ApiProperty({ example: 'user@gmail.com', required: true })
-  @IsEmail()
-  email: string;
-
-  @ApiProperty({ example: 'qwerty123', required: true, minLength: 8, maxLength: 30 })
-  @IsString()
-  @Length(8, 30)
-  password: string;
-}
+import { PickType } from '@nestjs/swagger';
+import { CreateUserDto } from 'src/users/dto';
+export class LoginUserDto extends PickType(CreateUserDto, ['email', 'password'] as const) {}
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,30 +1,10 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString, IsNotEmpty, Length, IsNumber, Min, Max } from 'class-validator';
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { IsString, Length, } from 'class-validator';
+import { PureUserDto } from './pure-user.dto';
 
-export class CreateUserDto {
-  @ApiProperty({example: 'user@gmail.com', required: true })
-  @IsEmail()
-  email: string;
-
+export class CreateUserDto extends PickType(PureUserDto, ['email', 'fullName', 'phone']) {
   @ApiProperty({  example: 'qwerty123', required: true, minLength: 8, maxLength: 30 })
   @IsString()
   @Length(8, 30)
   password: string;
-
-  @ApiProperty({ description: 'full name of user', example: 'Vladislav Makarov', required: true })
-  @IsString()
-  @IsNotEmpty()
-  fullName: string;
-
-  @ApiProperty({
-    description: 'Work phone number',
-    example: 89994377832,
-    required: true,
-    minimum: 100000,
-    maximum: 1000000000000000,
-  })
-  @IsNumber()
-  @Min(100000)
-  @Max(1000000000000000)
-  phone: number;
 }
