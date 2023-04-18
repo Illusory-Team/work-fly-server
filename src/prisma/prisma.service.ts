e063dbed4b7d8b812def39e36ab4594c7efed99c
Leahim <128914772+Leahim2085@users.noreply.github.com>
2023-04-18 21:53:18 +0300

    Update prisma.service.ts

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,6 +6,10 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
   }
+  
+  async onModuleDestroy() {    
+    await this.$disconnect();
+  }
 
   async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
